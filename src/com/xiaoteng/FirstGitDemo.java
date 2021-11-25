209ba7276342c48d5a07b2eee416128f2deffdc0
zxt <1147266739@qq.com>
2021-11-25 21:29:21 +0800

    first githubdemo

---
+++ src/com/xiaoteng/FirstGitDemo.java
@@ -0,0 +1,11 @@
+package com.xiaoteng;
+
+/**
+ * @author xiaozhuang
+ * @date 2021年11月25日 21:22
+ */
+public class FirstGitDemo {
+    public static void main(String[] args) {
+        System.out.println("第一个github程序");
+    }
+}
