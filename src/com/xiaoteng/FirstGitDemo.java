730691c8739f635fc9c868e65348907f34e3d486
rootzxt <94978013+rootzxt@users.noreply.github.com>
2021-11-25 21:51:25 +0800

    Update FirstGitDemo.java

--- src/com/xiaoteng/FirstGitDemo.java
+++ src/com/xiaoteng/FirstGitDemo.java
@@ -6,6 +6,6 @@ package com.xiaoteng;
  */
 public class FirstGitDemo {
     public static void main(String[] args) {
-        System.out.println("第一个github程序");
+        System.out.println("第一个github");
     }
 }
